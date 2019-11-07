a33d557083e9755e4d75215c56b5fe089a21631b
networkcv <networkcv@163.com>
2019-11-07 23:12:23 +0800

    update 多线程
    study
    CountDownLatch ReadWriteLock CyclicBarrier and LockSupport

---
+++ Java/Java并发/JDK并发包/countDownLatch/CountDownLatch1.java
@@ -0,0 +1,37 @@
+package 多线程;
+
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * create by lwj on 2019/11/7
+ */
+public class CountDownLatch1 {
+    static CountDownLatch end = new CountDownLatch(10);
+
+    @Test
+    public void test() {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < 10; i++) {
+            executorService.submit(new Thread(() -> {
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName()+" ok");
+                end.countDown();
+            }),i);
+        }
+        try {
+            end.await();
+            System.out.println("检查完毕");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
