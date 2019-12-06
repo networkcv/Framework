5c560fc9f62c72ebe874c24b2c65f30c64df19cb
networkcv <networkcv@163.com>
2019-12-06 23:30:28 +0800

    Update

--- Java/java_concurrency_practice/tmp/ThreadTest.java
+++ Java/java_concurrency_practice/tmp/ThreadTest.java
@@ -4,4 +4,64 @@ package tmp;
  * create by lwj on 2019/12/1
  */
 public class ThreadTest {
+    Object o = new Object();
+
+    public synchronized void m1() { // 重量级的访问操作。
+        System.out.println("public synchronized void m1() start");
+        try {
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("public synchronized void m1() end");
+    }
+
+    public void m3() {
+        synchronized (o) {
+            System.out.println("public void m3() start");
+            try {
+                Thread.sleep(30000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("public void m3() end");
+        }
+    }
+
+    public synchronized void m2() {
+        System.out.println("public void m2() start");
+        try {
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("public void m2() end");
+    }
+
+    public static class MyThread01 implements Runnable {
+        public MyThread01(int i, ThreadTest t) {
+            this.i = i;
+            this.t = t;
+        }
+
+        int i;
+        ThreadTest t;
+
+        public void run() {
+            if (i == 0) {
+                t.m1();
+            } else if (i > 0) {
+                t.m2();
+            } else {
+                t.m3();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ThreadTest t = new ThreadTest();
+        new Thread(new MyThread01(0, t)).start();
+        new Thread(new MyThread01(1, t)).start();
+        new Thread(new MyThread01(-1, t)).start();
+    }
 }
