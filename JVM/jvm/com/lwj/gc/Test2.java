be525cd43b0b0fc11976a47540c4632ab57ded57
networkcv <networkcv@163.com>
2020-02-21 19:41:32 +0800

    update 代码书写规范

--- JVM/jvm/com/lwj/gc/Test2.java
+++ JVM/jvm/com/lwj/gc/Test2.java
@@ -1,11 +1,25 @@
 package com.lwj.gc;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+
 /**
  * create by lwj on 2020/2/17
  */
-public class Test2 {
-    public static void main(String[] args) {
-        Object o = new Object();
+public class Test2 extends HashSet {
+    public int size = 0;
+
+
+    @Override
+    public boolean addAll(Collection c) {
+        return super.addAll(c);
+    }
+
+    public static void main(String[] args){
+        Test2 test2 = new Test2();
+        test2.addAll(Arrays.asList(1,2));
+        System.out.println(test2.size);
     }
 }
 
