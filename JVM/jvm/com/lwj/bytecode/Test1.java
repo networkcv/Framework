895de2e19d4e79d74b3a61e432e35e0371b65e03
networkcavalry <networkcavalry@gmail.com>
2020-02-25 18:26:54 +0800

    Add JVM：3-类加载器深入解析与阶段分解

--- JVM/jvm/com/lwj/bytecode/Test1.java
+++ JVM/jvm/com/lwj/bytecode/Test1.java
@@ -1,9 +1,21 @@
 package com.lwj.bytecode;
 
+
+import java.io.IOException;
+
 /**
  * create by lwj on 2020/2/24
  */
-public class Test1{
+public class Test1<C> {
+    String[]s;
+    private C c;
+
+    public void test(){
+    }
+    private C getC(){
+        return c;
+    }
+    public  final  static int b=1;
     private int a=1;
     public int getA(){
         return a;
@@ -11,4 +23,28 @@ public class Test1{
     public void setA(int a){
         this.a=a;
     }
+
+    public int exceptionTest()throws IOException ,Exception {
+        int i;
+        try{
+            i=1/0;
+            return i;
+        }catch (NullPointerException e){
+            i=2;
+            return i;
+        }finally {
+            i=3;
+            return i;
+        }
+    }
+    public class InnerClass{
+        int i=1;
+        private void innerFun(){
+            int i=2;
+            String s="3";
+            System.out.println(1);
+        }
+    }
+
+
 }
