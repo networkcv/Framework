34801eb21a4830cd0b1e939dae5c40db9005be8a
networkcv <networkcv@163.com>
2020-03-14 08:11:03 +0800

    Create yt-college

---
+++ Project/yt-college/src/main/java/com/zyt/ytcollege/YtCollegeApplication.java
@@ -0,0 +1,13 @@
+package com.zyt.ytcollege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtCollegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtCollegeApplication.class, args);
+	}
+
+}
---
+++ Project/yt-college/src/test/java/com/zyt/ytcollege/YtCollegeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyt.ytcollege;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtCollegeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
