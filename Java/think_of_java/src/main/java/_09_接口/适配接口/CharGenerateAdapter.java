7830590f843d5d02d7ee18c3e34a140c7dbc1255
networkcv <networkcv@163.com>
2020-03-17 08:04:43 +0800

    Update think of Java
    完成第9章接口阅读，理解适配器模式与策略模式

---
+++ Java/think_of_java/src/main/java/_09_接口/适配接口/CharGenerateAdapter.java
@@ -0,0 +1,29 @@
+package _09_接口.适配接口;
+
+import java.io.IOException;
+import java.nio.CharBuffer;
+import java.util.Scanner;
+
+/**
+ * create by lwj on 2020/3/16
+ */
+public class CharGenerateAdapter extends CharGenerate implements Readable {
+    public int count = 10;
+
+    @Override
+    public int read(CharBuffer cb) throws IOException {
+        if (count-- == 0) {
+            return -1;
+        }
+        cb.append(next());
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(new CharGenerateAdapter());
+        while (scanner.hasNext()) {
+            System.out.println(scanner.next());
+        }
+
+    }
+}
