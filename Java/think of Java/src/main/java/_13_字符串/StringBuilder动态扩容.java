75dc5855ac85efa9d0fec415b5a7e50e2a82fc09
networkcavalry <networkcavalry@gmail.com>
2020-04-06 18:33:49 +0800

    Update think of Java String & StringBuilder

---
+++ Java/think of Java/src/main/java/_13_字符串/StringBuilder动态扩容.java
@@ -0,0 +1,11 @@
+package _13_字符串;
+
+/**
+ * create by lwj on 2020/4/6
+ */
+public class StringBuilder动态扩容 {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("hello");
+    }
+}
