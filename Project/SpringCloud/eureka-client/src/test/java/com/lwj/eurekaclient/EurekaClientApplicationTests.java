15f30badaca2cf222822a0d7082b937c7977d04d
networkcavalry <networkcavalry@gmail.com>
2020-07-22 00:43:03 +0800

    Init SpringCloud Eureka-Client

---
+++ Project/SpringCloud/eureka-client/src/test/java/com/lwj/eurekaclient/EurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwj.eurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
