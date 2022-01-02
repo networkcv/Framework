01161b283288e33fa97157b56360b93921752453
networkcavalry <networkcavalry@gmail.com>
2022-01-02 16:21:29 +0800

    netty http unpacking

---
+++ Netty/netty/netty-demo/src/main/java/com/lwj/_09_netty_unpacking/NettyServerHandler.java
@@ -0,0 +1,97 @@
+package com.lwj._09_netty_unpacking;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+/**
+ * 自定义处理Handler
+ */
+public class NettyServerHandler implements ChannelInboundHandler {
+    public int count = 0;
+
+    /**
+     * 通道读取事件
+     *
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf byteBuf = (ByteBuf) msg;
+        System.out.println("客户端发送过来的消息:" + byteBuf.toString(CharsetUtil.UTF_8));
+        System.out.println("读取次数:"+(++count));
+    }
+
+
+    /**
+     * 通道读取完毕事件
+     *
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("你好.我是Netty服务端",
+                CharsetUtil.UTF_8));//消息出站
+    }
+
+    /**
+     * 通道异常事件
+     *
+     * @param ctx
+     * @param cause
+     * @throws Exception
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+
+    }
+
+    @Override
+    public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+
+}
