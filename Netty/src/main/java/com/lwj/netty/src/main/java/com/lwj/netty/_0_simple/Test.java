3e7d7d66e5e032354c7d7a79c08f20e02e1bf57d
networkcavalry <networkcavalry@gmail.com>
2022-01-10 11:44:42 +0800

    remove useless code

--- Netty/src/main/java/com/lwj/netty/src/main/java/com/lwj/netty/_0_simple/Test.java
+++
@@ -1,9 +0,0 @@
-package com.lwj.netty._0_simple;
-
-import io.netty.util.NettyRuntime;
-
-public class Test {
-   public static void main(String[] args) {
-        System.out.println(NettyRuntime.availableProcessors());
-    }
-}
