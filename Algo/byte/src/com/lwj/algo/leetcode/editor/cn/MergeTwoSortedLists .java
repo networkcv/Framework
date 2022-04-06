0782e8447410f7a758de3194093c616077de87da
networkcavalry <networkcavalry@gmail.com>
2022-04-06 22:03:36 +0800

    合并有序链表

---
+++ Algo/byte/src/com/lwj/algo/leetcode/editor/cn/MergeTwoSortedLists .java
@@ -0,0 +1,92 @@
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：l1 = [1,2,4], l2 = [1,3,4]
+//输出：[1,1,2,3,4,4]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：l1 = [], l2 = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：l1 = [], l2 = [0]
+//输出：[0]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 两个链表的节点数目范围是 [0, 50] 
+// -100 <= Node.val <= 100 
+// l1 和 l2 均按 非递减顺序 排列 
+// 
+// Related Topics 递归 链表 👍 2312 👎 0
+
+
+package com.lwj.algo.leetcode.editor.cn;
+
+class MergeTwoSortedLists {
+    public static void main(String[] args) {
+        Solution solution = new MergeTwoSortedLists().new Solution();
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(3);
+        ListNode l3 = new ListNode(5);
+        ListNode r1 = new ListNode(1);
+        ListNode r2 = new ListNode(2);
+        ListNode r3 = new ListNode(6);
+        l1.next = l2;
+        l2.next = l3;
+        r1.next = r2;
+        r2.next = r3;
+        System.out.println(solution.mergeTwoLists(l1, r1));
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            ListNode res = new ListNode();
+            ListNode cur = res;
+            while (list1 != null && list2 != null) {
+                if (list1.val < list2.val) {
+                    cur.next = list1;
+                    list1 = list1.next;
+                } else {
+                    cur.next = list2;
+                    list2 = list2.next;
+                }
+                cur = cur.next;
+            }
+            if (list1 == null) {
+                cur.next = list2;
+            }
+            if (list2 == null) {
+                cur.next = list1;
+            }
+            return res.next;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
