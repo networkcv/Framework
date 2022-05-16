09bd7d78ec5c336b464c8978f1851a2d684e1698
networkcavalry <networkcavalry@gmail.com>
2022-05-16 21:45:48 +0800

    爬楼梯和杨辉三角

---
+++ Algo/byte/src/com/lwj/algo/leetcode/editor/cn/ClimbingStairs .java
@@ -0,0 +1,70 @@
+//假设你正在爬楼梯。需要 n 阶你才能到达楼顶。 
+//
+// 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？ 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：n = 2
+//输出：2
+//解释：有两种方法可以爬到楼顶。
+//1. 1 阶 + 1 阶
+//2. 2 阶 
+//
+// 示例 2： 
+//
+// 
+//输入：n = 3
+//输出：3
+//解释：有三种方法可以爬到楼顶。
+//1. 1 阶 + 1 阶 + 1 阶
+//2. 1 阶 + 2 阶
+//3. 2 阶 + 1 阶
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= n <= 45 
+// 
+// Related Topics 记忆化搜索 数学 动态规划 👍 2425 👎 0
+
+
+package com.lwj.algo.leetcode.editor.cn;
+
+class ClimbingStairs {
+    public static void main(String[] args) {
+        Solution solution = new ClimbingStairs().new Solution();
+        for (int i = 0; i < 10; i++) {
+            try {
+                System.out.println(solution.climbStairs(i));
+            } catch (Exception e) {
+                System.out.println("error:" + i);
+            }
+        }
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int climbStairs(int n) {
+            if (n == 0) {
+                return 0;
+            } else if (n == 1) {
+                return 1;
+            }
+            int[] dp = new int[n + 1];
+            dp[1] = 1;
+            dp[2] = 2;
+            for (int i = 3; i <= n; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+            return dp[n];
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
