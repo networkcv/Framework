373b325c0069817274b5736987b61e0f1c9768ec
networkcavalry <networkcavalry@gmail.com>
2022-07-26 18:50:02 +0800

    algo 链表

---
+++ Algo/byte/src/com/lwj/algo/leetcode/editor/cn/LinkedListCycleLcci .java
@@ -0,0 +1,97 @@
+//给定一个链表，如果它是有环链表，实现一个算法返回环路的开头节点。若环不存在，请返回 null。 
+//
+// 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的
+//位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：head = [3,2,0,-4], pos = 1
+//输出：tail connects to node index 1
+//解释：链表中有一个环，其尾部连接到第二个节点。
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入：head = [1,2], pos = 0
+//输出：tail connects to node index 0
+//解释：链表中有一个环，其尾部连接到第一个节点。
+// 
+//
+// 示例 3： 
+//
+// 
+//
+// 
+//输入：head = [1], pos = -1
+//输出：no cycle
+//解释：链表中没有环。 
+//
+// 
+//
+// 进阶： 
+//
+// 
+// 你是否可以不用额外空间解决此题？ 
+// 
+//
+// 
+// Related Topics 哈希表 链表 双指针 👍 103 👎 0
+
+
+package com.lwj.algo.leetcode.editor.cn;
+
+class LinkedListCycleLcci {
+    public static void main(String[] args) {
+        Solution solution = new LinkedListCycleLcci().new Solution();
+        System.out.println(solution.detectCycle(ListNodeUtils.buildCycle(12345)));
+        System.out.println(solution.detectCycle(ListNodeUtils.buildCycle(12341)).val);
+        System.out.println(solution.detectCycle(ListNodeUtils.buildCycle(12342)).val);
+        System.out.println(solution.detectCycle(ListNodeUtils.buildCycle(12343)).val);
+
+
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) {
+     * val = x;
+     * next = null;
+     * }
+     * }
+     */
+    public class Solution {
+        public ListNode detectCycle(ListNode head) {
+            ListNode fast = head, slow = head;
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+                if (fast == slow) {
+                    break;
+                }
+            }
+            if (fast == null || fast.next == null) {
+                return null;
+            }
+            slow = head;
+            while (slow != fast) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return fast;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
--- Algo/byte/src/com/lwj/algo/leetcode/editor/cn/ListNodeUtils.java
+++ Algo/byte/src/com/lwj/algo/leetcode/editor/cn/ListNodeUtils.java
@@ -1,5 +1,8 @@
 package com.lwj.algo.leetcode.editor.cn;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Date: 2022/4/7
  * <p>
@@ -33,6 +36,28 @@ public class ListNodeUtils {
             cur = cur.next;
         }
         return res.next;
+    }
+
+    /**
+     * 根据数字构建链表，可以构建环形链表，相同的数字只会存在一个节点
+     */
+    public static ListNode buildCycle(Integer integer) {
+        String[] nums = integer.toString().split("");
+        Map<String, ListNode> numsMap = new HashMap<>();
+        String preNum = null;
+        for (String curNum : nums) {
+            ListNode cur;
+            if ((cur = numsMap.get(curNum)) == null) {
+                cur = new ListNode(Integer.parseInt(curNum));
+                numsMap.put(curNum, cur);
+            }
+            if (preNum != null) {
+                ListNode preNode = numsMap.get(preNum);
+                preNode.next = cur;
+            }
+            preNum = curNum;
+        }
+        return numsMap.get(nums[0]);
     }
 
 }
