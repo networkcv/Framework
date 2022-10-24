37665b8b7b3bdc35f7f0826c261e2eaa7c1b1467
networkcavalry <networkcavalry@gmail.com>
2022-10-24 12:59:54 +0800

    jvm

---
+++ Algo/byte/src/com/lwj/algo/leetcode/editor/cn/WordRectangleLcci .java
@@ -0,0 +1,44 @@
+//<p>给定一份单词的清单，设计一个算法，创建由字母组成的面积最大的矩形，其中每一行组成一个单词(自左向右)，每一列也组成一个单词(自上而下)。不要求这些单词在清单里连续出现，但要求所有行等长，所有列等高。</p>
+//
+//<p>如果有多个面积最大的矩形，输出任意一个均可。一个单词可以重复使用。</p>
+//
+//<p><strong>示例 1:</strong></p>
+//
+//<pre><strong>输入:</strong> <span><code>["this", "real", "hard", "trh", "hea", "iar", "sld"]</code></span>
+//<strong>输出:
+//</strong><span><code>[
+//&nbsp;  "this",
+//&nbsp;  "real",
+//&nbsp;  "hard"</code></span>
+//]</pre>
+//
+//<p><strong>示例 2:</strong></p>
+//
+//<pre><strong>输入:</strong> <span><code>["aa"]</code></span>
+//<strong>输出: </strong>["aa","aa"]</pre>
+//
+//<p><strong>说明：</strong></p>
+//
+//<ul> 
+// <li><code>words.length &lt;= 1000</code></li> 
+// <li><code>words[i].length &lt;= 100</code></li> 
+// <li>数据保证单词足够随机</li> 
+//</ul>
+//
+//<div><div>Related Topics</div><div><li>字典树</li><li>数组</li><li>字符串</li><li>回溯</li></div></div><br><div><li>👍 39</li><li>👎 0</li></div>
+  
+package com.lwj.algo.leetcode.editor.cn;
+
+class WordRectangleLcci{
+  public static void main(String[] args) {
+       Solution solution = new WordRectangleLcci().new Solution();
+  }
+  //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String[] maxRectangle(String[] words) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
