759ed1491f005b9768c2c73d51de9e78648f537a
networkcavalry <networkcavalry@gmail.com>
2023-09-06 01:26:59 +0800

    1

---
+++ Language/Python/py/099_/打开文件.py
@@ -0,0 +1,4 @@
+import subprocess
+
+filename = "工时统计.py"
+subprocess.call(["open", filename])
---
+++ Language/Python/py/1~15_python基础/06_函数和模块的使用.py
@@ -0,0 +1,35 @@
+# python的函数不需要重载，因为可以支持默认值和可变参数
+def fun(num=0):
+    if num != 0:
+        return num
+    return -1
+
+
+print(fun())
+print(fun(1))
+
+
+# 支持可变参数，用*变量来表示，传进来的是数组
+def fun1(*num):
+    for x in num:
+        print(x)
+
+
+fun1(1)  # output:1
+fun1(1, 2)  # output:1,2
+fun1(1, 2, 3)  # output:1,2,3
+
+# 后导入的包会覆盖前边的，从哪里导入什么
+from _06.model1 import foo
+
+foo()
+
+from _06.model2 import foo
+
+foo()
+
+import _06.model1 as m1
+import _06.model2 as m2
+
+m1.foo()
+m2.foo()
