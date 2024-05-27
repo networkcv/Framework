2fed6218b22e516475ceb1e5774af501a8a11ed0
networkcavalry <networkcavalry@gmail.com>
2024-05-27 20:35:05 +0800

    algo dance

--- Language/Python/py/1~15_python基础/code/_06/model1.py
+++
@@ -1,6 +0,0 @@
-def foo():
-    print('hello, world!')
-
-
-# python隐含变量，函数入口的名称才是__main__
-print("__name__ is %s " % __name__)
--- Language/Python/py/1~15_python基础/code/_06/model2.py
+++
@@ -1,4 +0,0 @@
-def foo():
-    print('goodbye, world!')
-    print('current import model is  %s' % __name__)
-
