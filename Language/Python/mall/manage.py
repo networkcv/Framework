e9a976ff4eab70a39ba35dd3d05cafd1f572f893
networkcavalry <networkcavalry@gmail.com>
2024-06-20 11:47:53 +0800

    python mall

--- Language/Python/mall/manage.py
+++ Language/Python/mall/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Run administrative tapisks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mall.settings')
     try:
         from django.core.management import execute_from_command_line
