542fae427f1e7c890bef9299b92746fd22e65f27
networkcavalry <networkcavalry@gmail.com>
2024-10-12 17:34:27 +0800

    目录调整

---
+++ Language/Python/py/test/a.py
@@ -0,0 +1,12 @@
+def algorithm(n: int) -> str:
+    a = 1  # +1
+    a = a + 1  # +1
+    a = a * 2  # +1
+    # 循环 n 次
+    for i in range(n):  # +1
+        print(0)  # +1
+
+    return "1"
+
+
+print(algorithm(1))
