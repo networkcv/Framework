81f4f7cd34d2282d0816a93d7032a0cf5e89e135
networkcavalry <networkcavalry@gmail.com>
2025-02-03 11:36:10 +0800

    两数之和

---
+++ Algo/byte/src/com/lwj/algo/leetcode/editor/cn/_1_两数之和.java
@@ -0,0 +1,87 @@
+/**
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那 两个 整数，并返回它们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+ * <p>
+ * 你可以按任意顺序返回答案。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ * <p>
+ * <p>
+ * 示例 3：
+ * <p>
+ * <p>
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 2 <= nums.length <= 10⁴
+ * -10⁹ <= nums[i] <= 10⁹
+ * -10⁹ <= target <= 10⁹
+ * 只会存在一个有效答案
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 进阶：你可以想出一个时间复杂度小于 O(n²) 的算法吗？
+ * <p>
+ * Related Topics数组 | 哈希表
+ * <p>
+ * 👍 19237, 👎 0bug 反馈 | 使用指南 | 更多配套插件
+ */
+
+package com.lwj.algo.leetcode.editor.cn;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+class TwoSum {
+    public static void main(String[] args) {
+        Solution solution = new TwoSum().new Solution();
+        System.out.println(Arrays.toString(solution.twoSum(new int[]{2, 7, 11, 15}, 9)));
+        System.out.println(Arrays.toString(solution.twoSum(new int[]{3, 2, 4}, 6)));
+        System.out.println(Arrays.toString(solution.twoSum(new int[]{3, 2, 4}, 11)));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            //期望的数字，对应索引下标
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                int num = nums[i];
+                int expect = target - num;
+                Integer idx = map.get(expect);
+                if (Objects.isNull(idx)) {
+                    map.put(num, i);
+                } else {
+                    return new int[]{idx, i};
+                }
+            }
+            return new int[]{-1, -1};
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
