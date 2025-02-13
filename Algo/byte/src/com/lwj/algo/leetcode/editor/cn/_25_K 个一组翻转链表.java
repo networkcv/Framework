95173560b157e4a861e5f52cb27cdc6b25c34bbc
networkcavalry <networkcavalry@gmail.com>
2025-02-13 22:56:13 +0800

    K个一组翻转链表

--- Algo/byte/src/com/lwj/algo/leetcode/editor/cn/_25_K 个一组翻转链表.java
+++ Algo/byte/src/com/lwj/algo/leetcode/editor/cn/_25_K 个一组翻转链表.java
@@ -1,71 +1,61 @@
-//给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。 
-//
-// k 是一个正整数，它的值小于或等于链表的长度。 
-//
-// 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。 
-//
-// 进阶： 
-//
-// 
-// 你可以设计一个只使用常数额外空间的算法来解决此问题吗？ 
-// 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。 
-// 
-//
-// 
-//
-// 示例 1： 
-//
-// 
-//输入：head = [1,2,3,4,5], k = 2
-//输出：[2,1,4,3,5]
-// 
-//
-// 示例 2： 
-//
-// 
-//输入：head = [1,2,3,4,5], k = 3
-//输出：[3,2,1,4,5]
-// 
-//
-// 示例 3： 
-//
-// 
-//输入：head = [1,2,3,4,5], k = 1
-//输出：[1,2,3,4,5]
-// 
-//
-// 示例 4： 
-//
-// 
-//输入：head = [1], k = 1
-//输出：[1]
-// 
-//
-// 
-// 
-//
-// 提示： 
-//
-// 
-// 列表中节点的数量在范围 sz 内 
-// 1 <= sz <= 5000 
-// 0 <= Node.val <= 1000 
-// 1 <= k <= sz 
-// 
-// Related Topics 递归 链表 👍 1574 👎 0
-
-
 package com.lwj.algo.leetcode.editor.cn;
 
 import com.lwj.algo.leetcode.editor.cn.utils.ListNode;
 import com.lwj.algo.leetcode.editor.cn.utils.ListNodeUtils;
 
+/**
+ * 给你链表的头节点 head ，每 k 个节点一组进行翻转，请你返回修改后的链表。
+ * <p>
+ * k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+ * <p>
+ * 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4,5], k = 2
+ * 输出：[2,1,4,3,5]
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4,5], k = 3
+ * 输出：[3,2,1,4,5]
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 链表中的节点数目为 n
+ * 1 <= k <= n <= 5000
+ * 0 <= Node.val <= 1000
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 进阶：你可以设计一个只用 O(1) 额外内存空间的算法解决此问题吗？
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * Related Topics递归 | 链表
+ * <p>
+ * 👍 2474, 👎 0bug 反馈 | 使用指南 | 更多配套插件
+ */
 class ReverseNodesInKGroup {
     public static void main(String[] args) {
         Solution solution = new ReverseNodesInKGroup().new Solution();
-        System.out.println(solution.reverseKGroup(ListNodeUtils.build(2143), 2));
-        System.out.println(solution.reverseKGroup(ListNodeUtils.build(21435), 2));
+        ListNode listNode = ListNodeUtils.buildByArray(1, 2, 3, 4, 5);
+        System.out.println(solution.reverseKGroup(listNode, 2));
+        System.out.println(solution);
     }
+
     //leetcode submit region begin(Prohibit modification and deletion)
 
     /**
@@ -79,8 +69,43 @@ class ReverseNodesInKGroup {
      * }
      */
     class Solution {
-
+        //2025/2/13
+        //参考 https://www.bilibili.com/video/BV1sd4y1x7KN/?spm_id_from=333.788.player.switch&vd_source=1c3ddffa7c4adab111124a27aaa320a6
         public ListNode reverseKGroup(ListNode head, int k) {
+            ListNode tmp = head;
+            //链表长度
+            int len = 0;
+            while (tmp != null) {
+                len++;
+                tmp = tmp.next;
+            }
+            ListNode dummy = new ListNode(-1);
+            dummy.next = head;
+            //正在翻转的这组链表的前一个节点
+            ListNode p0 = dummy;
+            ListNode pre = dummy;
+            ListNode cur = head;
+            while (len - k >= 0) {
+                len = len - k;
+                int n = 0;
+                //翻转一组链表节点  -1 [1,2,3,4,5], k = 2
+                while (n++ < k) {
+                    ListNode next = cur.next;
+                    cur.next = pre;
+                    pre = cur;
+                    cur = next;
+                }
+                p0.next.next = cur;
+                ListNode nextP0 = p0.next;
+                p0.next = pre;
+                p0 = nextP0;
+            }
+            return dummy.next;
+
+        }
+
+        //2022/7/27
+        public ListNode reverseKGroup1(ListNode head, int k) {
             if (head == null) {
                 return null;
             }
