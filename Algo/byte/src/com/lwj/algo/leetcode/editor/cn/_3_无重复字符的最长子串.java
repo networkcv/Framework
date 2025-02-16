e68f904d83da6fc77a0c47d585dd003d1d9aad80
networkcavalry <networkcavalry@gmail.com>
2025-02-16 23:20:15 +0800

    todo

--- Algo/byte/src/com/lwj/algo/leetcode/editor/cn/_3_无重复字符的最长子串.java
+++ Algo/byte/src/com/lwj/algo/leetcode/editor/cn/_3_无重复字符的最长子串.java
@@ -5,13 +5,34 @@ import java.util.HashMap;
 class LongestSubstringWithoutRepeatingCharacters {
     public static void main(String[] args) {
         Solution solution = new LongestSubstringWithoutRepeatingCharacters().new Solution();
-        System.out.println(solution.lengthOfLongestSubstring("pwwkew"));
+        System.out.println(solution.lengthOfLongestSubstring("wwkew"));
+//        System.out.println(solution.lengthOfLongestSubstring("pwwkew"));
 //        System.out.println(solution.lengthOfLongestSubstring("a"));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+        //// TODO: 乌柏 2025/2/16  error
         public int lengthOfLongestSubstring(String s) {
+            HashMap<Character, Integer> map = new HashMap<>();
+            int res = 0;
+            int l = 0, r = 0;
+            int len = s.length();
+            while (r < len) {
+                char c = s.charAt(r);
+                r++;
+                map.merge(c, 1, Integer::sum);
+                while (map.get(c) > 1) {
+                    l++;
+                    map.computeIfPresent(c, (k, v) -> v - 1);
+                }
+                res = Math.max(res, r - l);
+
+            }
+            return res;
+        }
+
+        public int lengthOfLongestSubstring1(String s) {
             int res = 0;
             int l = 0, r = 0;
             HashMap<Character, Integer> map = new HashMap<>();
