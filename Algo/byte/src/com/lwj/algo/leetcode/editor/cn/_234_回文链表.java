39ef25a66724a3db6b44edacd04957bf78c7d1bc
networkcavalry <networkcavalry@gmail.com>
2025-02-20 12:41:50 +0800

    回文链表

--- Algo/byte/src/com/lwj/algo/leetcode/editor/cn/_234_回文链表.java
+++ Algo/byte/src/com/lwj/algo/leetcode/editor/cn/_234_回文链表.java
@@ -1,47 +1,51 @@
-//给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。 
-//
-// 
-//
-// 示例 1： 
-//
-// 
-//输入：head = [1,2,2,1]
-//输出：true
-// 
-//
-// 示例 2： 
-//
-// 
-//输入：head = [1,2]
-//输出：false
-// 
-//
-// 
-//
-// 提示： 
-//
-// 
-// 链表中节点数目在范围[1, 10⁵] 内 
-// 0 <= Node.val <= 9 
-// 
-//
-// 
-//
-// 进阶：你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？ 
-// Related Topics 栈 递归 链表 双指针 👍 1454 👎 0
-
-
 package com.lwj.algo.leetcode.editor.cn;
 
 import com.lwj.algo.leetcode.editor.cn.utils.ListNode;
 import com.lwj.algo.leetcode.editor.cn.utils.ListNodeUtils;
 
+import java.util.Stack;
+
+/**
+ * 给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [1,2,2,1]
+ * 输出：true
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：head = [1,2]
+ * 输出：false
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 链表中节点数目在范围[1, 10⁵] 内
+ * 0 <= Node.val <= 9
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 进阶：你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+ * <p>
+ * Related Topics栈 | 递归 | 链表 | 双指针
+ * <p>
+ * 👍 2007, 👎 0bug 反馈 | 使用指南 | 更多配套插件
+ */
 class PalindromeLinkedList {
     public static void main(String[] args) {
         Solution solution = new PalindromeLinkedList().new Solution();
-//        System.out.println(solution.isPalindrome(ListNodeUtils.build(1221)));
-        System.out.println(solution.isPalindrome(ListNodeUtils.build(121)));
-        System.out.println(solution.isPalindrome(ListNodeUtils.build(1222)));
+//        System.out.println(solution.isPalindrome(ListNodeUtils.build(101)));
+        System.out.println(solution.isPalindrome(ListNodeUtils.build(1221)));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
@@ -58,10 +62,37 @@ class PalindromeLinkedList {
      */
     class Solution {
 
+        //使用栈压入前半段元素，然后出栈比对后半段元素
+        public boolean isPalindrome(ListNode head) {
+            Stack<Integer> stack = new Stack<>();
+            ListNode q = head;
+            ListNode s = head;
+            if (head != null && head.next == null) {
+                return true;
+            }
+            while (q != null && q.next != null) {
+                stack.push(s.val);
+                q = q.next.next;
+                s = s.next;
+            }
+            //处理奇数链表,奇数链表遍历后q指向最后一个元素，偶数链表遍历后q指向null
+            if (q != null) {
+                stack.push(s.val);
+            }
+            while (s != null) {
+                if (s.val != stack.pop()) {
+                    return false;
+                }
+                s = s.next;
+            }
+            return true;
+        }
+
         ListNode left;
 
+        //2022/7/27
         //模仿双指针实现回文判断,巧妙使用递归压栈，从后往前遍历
-        public boolean isPalindrome2(ListNode head) {
+        public boolean isPalindrome1(ListNode head) {
             left = head;
             return traverse(head);
         }
@@ -75,8 +106,9 @@ class PalindromeLinkedList {
             return res;
         }
 
+        //2022/7/27
         //寻找中点，反转后半段，再进行比较
-        public boolean isPalindrome(ListNode head) {
+        public boolean isPalindrome2(ListNode head) {
             ListNode f = head, s = head;
             while (f != null && f.next != null) {
                 f = f.next.next;
@@ -88,29 +120,34 @@ class PalindromeLinkedList {
             }
             //反转链表
             ListNode newHead = reverseListNode(s);
+            return travelListNodeEquals(head, newHead);
+        }
+
+        /**
+         * 遍历比较链表节点是否一致
+         *
+         * @param head1 head1
+         * @param head2 head2
+         * @return 是否一致
+         */
+        private boolean travelListNodeEquals(ListNode head1, ListNode head2) {
             //进行比较
-            while (newHead != null) {
-                if (newHead.val != head.val) {
+            while (head2 != null) {
+                if (head2.val != head1.val) {
                     return false;
                 }
-                newHead = newHead.next;
-                head = head.next;
+                head2 = head2.next;
+                head1 = head1.next;
             }
             return true;
         }
 
+        //2022/7/27
         //先复制链表，再反转整个链表，再进行比较
         public boolean isPalindrome3(ListNode head) {
             ListNode copyHead = copy(head);
             ListNode newHead = reverseListNode(copyHead);
-            while (newHead != null) {
-                if (newHead.val != head.val) {
-                    return false;
-                }
-                head = head.next;
-                newHead = newHead.next;
-            }
-            return true;
+            return travelListNodeEquals(head, newHead);
         }
 
         private ListNode copy(ListNode head) {
