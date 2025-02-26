c06f7a2673f1b433c41dac91489d4cb159249d2c
networkcavalry <networkcavalry@gmail.com>
2025-02-26 11:51:41 +0800

    _198_打家劫舍 回溯版-暴力回溯

--- Algo/byte/src/com/lwj/algo/leetcode/editor/cn/_198_打家劫舍.java
+++ Algo/byte/src/com/lwj/algo/leetcode/editor/cn/_198_打家劫舍.java
@@ -1,5 +1,8 @@
 package com.lwj.algo.leetcode.editor.cn;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 //<p>你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，<strong>如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警</strong>。</p>
 //
 //<p>给定一个代表每个房屋存放金额的非负整数数组，计算你<strong> 不触动警报装置的情况下 </strong>，一夜之内能够偷窃到的最高金额。</p>
@@ -36,24 +39,38 @@ package com.lwj.algo.leetcode.editor.cn;
 class HouseRobber {
     public static void main(String[] args) {
         Solution solution = new HouseRobber().new Solution();
-//        System.out.println(solution.rob(new int[]{1, 2, 3, 1}));
-        System.out.println(solution.rob(new int[]{2, 1, 1, 2}));
+//        int[] param = new int[]{183, 219, 57, 193, 94, 233, 202, 154, 65, 240, 97, 234, 100, 249, 186, 66, 90, 238, 168, 128, 177, 235, 50, 81, 185, 165, 217, 207, 88, 80, 112, 78, 135, 62, 228, 247, 211};
+        int[] param = new int[]{1, 2, 3, 1};
+        System.out.println(solution.rob(param));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int rob(int[] nums) {
-            if (nums == null || nums.length == 0) return 0;
-            if (nums.length == 1) return nums[0];
-            if (nums.length == 2) return Math.max(nums[0], nums[1]);
-            int[] dp = new int[nums.length];
-            dp[0] = nums[0];
-            dp[1] = Math.max(nums[1], nums[0]);
-            for (int i = 2; i < nums.length; i++) {
-                dp[i]=Math.max(dp[i-2]+nums[i], dp[i-1]);
-            }
-            return dp[nums.length-1];
+            return rob2(nums);
         }
+
+        //回溯版-暴力回溯
+        public int rob0(int[] nums) {
+            dfs0(nums.length - 1, nums);
+            return res;
+        }
+
+        int res = 0;
+        LinkedList<Integer> path = new LinkedList<>();
+
+        public void dfs0(int i, int[] nums) {
+            if (i < 0) {
+                Integer sum = path.stream().reduce(Integer::sum).orElse(0);
+                res = Math.max(res, sum);
+                return;
+            }
+            dfs0(i - 1, nums);
+            path.add(nums[i]);
+            dfs0(i - 2, nums);
+            path.removeLast();
+        }
+
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
