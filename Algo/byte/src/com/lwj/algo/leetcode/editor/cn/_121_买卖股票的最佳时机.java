02771eb9849aadb6209c209de6716b076a7f62ef
networkcavalry <networkcavalry@gmail.com>
2025-03-03 17:04:26 +0800

    _121_买卖股票的最佳时机 (只交易一次) 回溯版-记忆化搜索

--- Algo/byte/src/com/lwj/algo/leetcode/editor/cn/_121_买卖股票的最佳时机.java
+++ Algo/byte/src/com/lwj/algo/leetcode/editor/cn/_121_买卖股票的最佳时机.java
@@ -1,5 +1,7 @@
 package com.lwj.algo.leetcode.editor.cn;
 
+import java.util.Arrays;
+
 //<p>给定一个数组 <code>prices</code> ，它的第&nbsp;<code>i</code> 个元素&nbsp;<code>prices[i]</code> 表示一支给定股票第 <code>i</code> 天的价格。</p>
 //
 //<p>你只能选择 <strong>某一天</strong> 买入这只股票，并选择在 <strong>未来的某一个不同的日子</strong> 卖出该股票。设计一个算法来计算你所能获取的最大利润。</p>
@@ -38,13 +40,42 @@ package com.lwj.algo.leetcode.editor.cn;
 class BestTimeToBuyAndSellStock {
     public static void main(String[] args) {
         Solution solution = new BestTimeToBuyAndSellStock().new Solution();
-        System.out.println(solution);
+        System.out.println(solution.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        //枚举保存最值
+        int[] prices;
+        int[][][] cache;
+
         public int maxProfit(int[] prices) {
+            this.prices = prices;
+            cache = new int[prices.length + 1][3][2];
+            for (int[][] ints : cache) {
+                for (int[] anInt : ints) {
+                    Arrays.fill(anInt, Integer.MIN_VALUE);
+                }
+            }
+            return dfs(prices.length - 1, 1, false);
+        }
+
+        //回溯版-记忆化搜索
+        public int dfs(int i, int n, boolean hold) {
+            if (n < 0) return Integer.MIN_VALUE;
+            if (i < 0) {
+                return hold ? Integer.MIN_VALUE : 0;
+            }
+            if (cache[i][n][hold ? 1 : 0] != Integer.MIN_VALUE) return cache[i][n][hold ? 1 : 0];
+            if (hold) {
+                //不操作，或者买入
+                return cache[i][n][1] = Math.max(dfs(i - 1, n, true), dfs(i - 1, n, false) - prices[i]);
+            }
+            //不操作，或者卖出
+            return cache[i][n][0] = Math.max(dfs(i - 1, n, false), dfs(i - 1, n - 1, true) + prices[i]);
+        }
+
+        //枚举保存最值
+        public int maxProfit0(int[] prices) {
             int res = 0;
             int min = prices[0];
             for (int price : prices) {
