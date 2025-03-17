dd2cb167cefd3ff135c06b1b4ce7c0899579ed08
networkcavalry <networkcavalry@gmail.com>
2025-03-17 11:58:28 +0800

    SkipList 跳表

--- Algo/byte/src/com/lwj/algo/leetcode/editor/cn/custom/_09_跳表/SkipList.java
+++ Algo/byte/src/com/lwj/algo/leetcode/editor/cn/custom/_09_跳表/SkipList.java
@@ -1,5 +1,6 @@
 package com.lwj.algo.leetcode.editor.cn.custom._09_跳表;
 
+import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -12,11 +13,70 @@ public class SkipList {
 
     public static final int MAX_LEVEL = 3;
 
+    private int size = 0;
+
     private int levelCount = 1;
 
     private Node head = new Node();  // 带头链表
 
     private Random r = new Random();
+
+    public void insert2(int value) {
+        Node[] update;
+        if (size % 2 == 0) {
+            update = new Node[2];
+        } else {
+            update = new Node[1];
+        }
+        Arrays.fill(update, head);
+
+        for (int i = 0; i < update.length; i++) {
+            //将update[i]遍历至value节点前一个
+            while (update[i].forwards[i] != null && update[i].forwards[i].data < value) {
+                update[i] = update[i].forwards[i];
+            }
+        }
+        Node newNode = new Node();
+        newNode.data = value;
+        for (int i = 0; i < update.length; i++) {
+            //先记录下一个节点
+            newNode.forwards[i] = update[i].forwards[i];
+            //再将当前节点的下一个节点指向newNode
+            update[i].forwards[i] = newNode;
+        }
+        size++;
+    }
+
+    public Node find2(int value) {
+        Node pre = head;
+        //先从快速线查找
+        for (int i = head.forwards.length - 1; i >= 0; i--) {
+            while (pre.forwards[i] != null && pre.forwards[i].data < value) {
+                pre = pre.forwards[i];
+            }
+        }
+        if (pre.forwards[0] == null || pre.forwards[0].data != value) return null;
+        return pre.forwards[0];
+    }
+
+    public void delete2(int value) {
+        Node[] update = new Node[2];
+        Arrays.fill(update, head);
+        for (int i = 0; i < update.length; i++) {
+            while (update[i].forwards[i] != null && update[i].forwards[i].data < value) {
+                update[i] = update[i].forwards[i];
+            }
+        }
+        for (int i = 0; i < update.length; i++) {
+            //当前层不存在value值
+            if (update[i].forwards[i] == null || update[i].forwards[i].data != value) {
+                return;
+            }
+            Node nextNextNode = update[i].forwards[i].forwards[i];
+            update[i].forwards[i] = nextNextNode;
+        }
+        size--;
+    }
 
     public Node find(int value) {
         Node p = head;
@@ -38,14 +98,12 @@ public class SkipList {
         Node newNode = new Node();
         newNode.data = value;
         newNode.maxLevel = level;
-        Node update[] = new Node[level];
-        for (int i = 0; i < level; ++i) {
-            update[i] = head;
-        }
+        Node[] update = new Node[level];
+        Arrays.fill(update, head);
 
         // record every level largest value which smaller than insert value in update[]
-        Node p = head;
         for (int i = level - 1; i >= 0; --i) {
+            Node p = head;
             while (p.forwards[i] != null && p.forwards[i].data < value) {
                 p = p.forwards[i];
                 update[i] = p;// use update save node in search path
--- Algo/byte/src/com/lwj/algo/leetcode/editor/cn/custom/_09_跳表/Test2.java
+++ Algo/byte/src/com/lwj/algo/leetcode/editor/cn/custom/_09_跳表/Test2.java
@@ -7,14 +7,15 @@ package com.lwj.algo.leetcode.editor.cn.custom._09_跳表;
 public class Test2 {
     public static void main(String[] args) {
         SkipList skipList = new SkipList();
-        skipList.insert(1, 2);
-        skipList.insert(5, 3);
-        skipList.insert(2, 1);
-        skipList.insert(4, 2);
-        System.out.println(skipList.find(6));
-        skipList.insert(3, 2);
-        System.out.println(skipList.find(3));
-        skipList.delete(3);
+        skipList.insert2(1);
+        skipList.insert2(2);
+        skipList.insert2(3);
+        skipList.insert2(4);
+        skipList.insert2(5);
+        System.out.println(skipList.find2(6));
+        System.out.println(skipList.find2(3));
+        skipList.delete2(3);
+        System.out.println(skipList.find2(3));
     }
 
 }
