992ebb20dd986acc3512fcabc964026ac00ca3d3
networkcavalry <networkcavalry@gmail.com>
2025-03-17 15:53:30 +0800

    _94_二叉树的中序遍历

---
+++ Algo/byte/src/com/lwj/algo/leetcode/editor/cn/_94_二叉树的中序遍历.java
@@ -0,0 +1,89 @@
+package com.lwj.algo.leetcode.editor.cn;
+
+import com.lwj.algo.leetcode.editor.cn.utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//<p>给定一个二叉树的根节点 <code>root</code> ，返回 <em>它的 <strong>中序</strong>&nbsp;遍历</em> 。</p>
+//
+//<p>&nbsp;</p>
+//
+//<p><strong>示例 1：</strong></p> 
+//<img alt="" src="https://assets.leetcode.com/uploads/2020/09/15/inorder_1.jpg" style="height: 200px; width: 125px;" /> 
+//<pre>
+//<strong>输入：</strong>root = [1,null,2,3]
+//<strong>输出：</strong>[1,3,2]
+//</pre>
+//
+//<p><strong>示例 2：</strong></p>
+//
+//<pre>
+//<strong>输入：</strong>root = []
+//<strong>输出：</strong>[]
+//</pre>
+//
+//<p><strong>示例 3：</strong></p>
+//
+//<pre>
+//<strong>输入：</strong>root = [1]
+//<strong>输出：</strong>[1]
+//</pre>
+//
+//<p>&nbsp;</p>
+//
+//<p><strong>提示：</strong></p>
+//
+//<ul> 
+// <li>树中节点数目在范围 <code>[0, 100]</code> 内</li> 
+// <li><code>-100 &lt;= Node.val &lt;= 100</code></li> 
+//</ul>
+//
+//<p>&nbsp;</p>
+//
+//<p><strong>进阶:</strong>&nbsp;递归算法很简单，你可以通过迭代算法完成吗？</p>
+//
+//<div><div>Related Topics</div><div><li>栈</li><li>树</li><li>深度优先搜索</li><li>二叉树</li></div></div><br><div><li>👍 2211</li><li>👎 0</li></div>
+class BinaryTreeInorderTraversal {
+    public static void main(String[] args) {
+        Solution solution = new BinaryTreeInorderTraversal().new Solution();
+        System.out.println(solution);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        List<Integer> res = new ArrayList<>();
+
+        public List<Integer> inorderTraversal(TreeNode root) {
+            travel(root);
+            return res;
+        }
+
+        public void travel(TreeNode root) {
+            if (root == null) {
+                return;
+            }
+            travel(root.left);
+            res.add(root.val);
+            travel(root.right);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
