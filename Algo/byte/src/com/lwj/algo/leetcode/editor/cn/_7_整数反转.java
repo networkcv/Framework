5df9142f6818090b3ec87cc523715f9923666926
networkcavalry <networkcavalry@gmail.com>
2025-07-18 15:09:06 +0800

    remove history code

--- Algo/byte/src/com/lwj/algo/leetcode/editor/cn/_7_整数反转.java
+++ Algo/byte/src/com/lwj/algo/leetcode/editor/cn/_7_整数反转.java
@@ -1,56 +1,54 @@
 package com.lwj.algo.leetcode.editor.cn;
 
-//<p>给你一个 32 位的有符号整数 <code>x</code> ，返回将 <code>x</code> 中的数字部分反转后的结果。</p>
-//
-//<p>如果反转后整数超过 32 位的有符号整数的范围&nbsp;<code>[−2<sup>31</sup>,&nbsp; 2<sup>31&nbsp;</sup>− 1]</code> ，就返回 0。</p> 
-//<strong>假设环境不允许存储 64 位整数（有符号或无符号）。</strong>
-//
-//<p>&nbsp;</p>
-//
-//<p><strong>示例 1：</strong></p>
-//
-//<pre>
-//<strong>输入：</strong>x = 123
-//<strong>输出：</strong>321
-//</pre>
-//
-//<p><strong>示例 2：</strong></p>
-//
-//<pre>
-//<strong>输入：</strong>x = -123
-//<strong>输出：</strong>-321
-//</pre>
-//
-//<p><strong>示例 3：</strong></p>
-//
-//<pre>
-//<strong>输入：</strong>x = 120
-//<strong>输出：</strong>21
-//</pre>
-//
-//<p><strong>示例 4：</strong></p>
-//
-//<pre>
-//<strong>输入：</strong>x = 0
-//<strong>输出：</strong>0
-//</pre>
-//
-//<p>&nbsp;</p>
-//
-//<p><strong>提示：</strong></p>
-//
-//<ul> 
-// <li><code>-2<sup>31</sup> &lt;= x &lt;= 2<sup>31</sup> - 1</code></li> 
-//</ul>
-//
-//<div><div>Related Topics</div><div><li>数学</li></div></div><br><div><li>👍 4095</li><li>👎 0</li></div>
+/**
+ * <p>给你一个 32 位的有符号整数 <code>x</code> ，返回将 <code>x</code> 中的数字部分反转后的结果。</p>
+ *
+ * <p>如果反转后整数超过 32 位的有符号整数的范围&nbsp;<code>[−2<sup>31</sup>,&nbsp; 2<sup>31&nbsp;</sup>− 1]</code> ，就返回 0。</p>
+ * <strong>假设环境不允许存储 64 位整数（有符号或无符号）。</strong>
+ *
+ * <p>&nbsp;</p>
+ *
+ * <p><strong>示例 1：</strong></p>
+ *
+ * <pre>
+ * <strong>输入：</strong>x = 123
+ * <strong>输出：</strong>321
+ * </pre>
+ *
+ * <p><strong>示例 2：</strong></p>
+ *
+ * <pre>
+ * <strong>输入：</strong>x = -123
+ * <strong>输出：</strong>-321
+ * </pre>
+ *
+ * <p><strong>示例 3：</strong></p>
+ *
+ * <pre>
+ * <strong>输入：</strong>x = 120
+ * <strong>输出：</strong>21
+ * </pre>
+ *
+ * <p><strong>示例 4：</strong></p>
+ *
+ * <pre>
+ * <strong>输入：</strong>x = 0
+ * <strong>输出：</strong>0
+ * </pre>
+ *
+ * <p>&nbsp;</p>
+ *
+ * <p><strong>提示：</strong></p>
+ *
+ * <ul>
+ * <li><code>-2<sup>31</sup> &lt;= x &lt;= 2<sup>31</sup> - 1</code></li>
+ * </ul>
+ *
+ * <div><div>Related Topics</div><div><li>数学</li></div></div><br><div><li>👍 4095</li><li>👎 0</li></div>
+ */
 class ReverseInteger {
     public static void main(String[] args) {
         Solution solution = new ReverseInteger().new Solution();
-        System.out.println(11 / 10);
-        System.out.println(11 % 10);
-        System.out.println(-11 / 10);
-        System.out.println(-11 % 10);
 //        System.out.println(solution.reverse(-123));
 //        System.out.println(solution.reverse(123));
 //        System.out.println(solution.reverse(120));
